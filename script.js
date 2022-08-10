2817c614205ba5f305c8d90cd808d49199ec0c75
Shannon Brooks <sbrooks0084@gmail.com>
2022-08-10 09:19:07 -0500

    completed

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Learning Chart.js')
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+  type: 'bar',
+  data: {
+    labels: ["United States", "Spain", "United Kingdom", "Germany", "France", "Democratic Republic Of The Congo", "Nigeria", "Ghana"],
+      datasets: [{
+        label: 'Number of Cases',
+        data: [8.933, 4.942, 2.859, 2.916, 2.423, 0.163, 0.133, 0.030],
+        backgroundColor: "rgba(255,153,0,0.4)"
+        
+    },]
+
+  }
+});
